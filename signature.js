dbdac67d321af442c9425fc753d678880a009790
Kauan Xavier <143226394+kauanodev@users.noreply.github.com>
2025-01-17 17:01:08 -0300

    Atualizações 1701
    Style.css criado e problema com as assinaturas corrigido.

--- signature.js
+++ signature.js
@@ -1,83 +1,87 @@
-async function canvas_control(div) {
-  var largura = 500;
-  var altura = 300;
-  var desenhando = false;
-  console.log(div);
-  quadro = div.querySelector("canvas");
-  console.log(quadro);
-  quadro.setAttribute("width", largura);
-  quadro.setAttribute("height", altura);
-  var limpar = div.querySelector("button");
-  console.log(limpar);
-
-  var ctx = quadro.getContext("2d");
-
-  quadro.onmousedown = function (evt) {
-    var coordenadas_quadro = posicao(quadro);
-    ctx.moveTo(
-      evt.clientX - coordenadas_quadro.x,
-      evt.clientY - coordenadas_quadro.y
-    );
-    desenhando = true;
-  };
-
-  quadro.touchstart = function (evt) {
-    console.log("tocou");
-    var coordenadas_quadro = posicao(quadro);
-    ctx.moveTo(
-      evt.clientX - coordenadas_quadro.x,
-      evt.clientY - coordenadas_quadro.y
-    );
-    desenhando = true;
-  };
-
-  quadro.onmouseup = function () {
-    desenhando = false;
-  };
-  quadro.touchend = function () {
-    desenhando = false;
-  };
-
-  quadro.onmousemove = function (evt) {
-    if (desenhando) {
-      // ctx.lineTo(evt.clientX, evt.clientY-175);
-      var coordenadas_quadro = posicao(quadro);
-
-      ctx.lineTo(
-        evt.clientX - coordenadas_quadro.x,
-        evt.clientY - coordenadas_quadro.y
-      );
-      ctx.stroke();
-      console.log(quadro.dataset);
-    }
-  };
-
-  limpar.onclick = function () {
-    ctx.beginPath();
-    ctx.clearRect(0, 0, largura, altura);
-    quadro.dataset = {};
-    console.log(quadro.dataset);
-  };
-
-  function posicao(obj) {
-    var coordenadas = obj.getBoundingClientRect();
-    var x = coordenadas.left;
-    var y = coordenadas.top;
-    console.log("posição x", x, "posição y", y);
-    return {
-      x: x,
-      y: y,
-    };
-  }
-}
-
 //consertar paralelismo
 window.onload = function () {
   var signatureClient = document.getElementById("divSignatureClient");
   var signatureTechnician = document.getElementById("divSignatureTechnician");
-  if(signatureClient.mousedown()){
-    canvas_control(signatureClient);
-  }
-  else{canvas_control(signatureTechnician);}
   
+  function canvas_control(div) {
+    var largura = "75vw";
+    var altura = "300px";
+    var desenhando = false;
+    var quadro = div.querySelector("canvas");
+    var limpar = div.querySelector("button");
+    var ctx = quadro.getContext("2d");
+    quadro.setAttribute("width", largura);
+    quadro.setAttribute("height", altura);
+
+    quadro.onmousedown = function (evt) {
+      var coordenadas_quadro = posicao(quadro);
+      ctx.moveTo(
+        evt.clientX - coordenadas_quadro.x,
+        evt.clientY - coordenadas_quadro.y
+      );
+      desenhando = true;
+    };
+
+
+    quadro.touchstart = function (evt) {
+
+      var coordenadas_quadro = posicao(quadro);
+      ctx.moveTo(
+        evt.clientX - coordenadas_quadro.x,
+        evt.clientY - coordenadas_quadro.y
+      );
+      desenhando = true;
+    };
+
+
+    quadro.onmouseup = function () {
+      desenhando = false;
+    };
+
+
+    quadro.touchend = function () {
+      desenhando = false;
+    };
+
+
+    quadro.onmousemove = function (evt) {
+      if (desenhando) {
+        // ctx.lineTo(evt.clientX, evt.clientY-175);
+        var coordenadas_quadro = posicao(quadro);
+
+        ctx.lineTo(
+          evt.clientX - coordenadas_quadro.x,
+          evt.clientY - coordenadas_quadro.y
+        );
+        ctx.stroke();
+      }
+    };
+   
+
+    limpar.onclick = function () {
+     //#TODO fazer com que as medidas do canva sejam atribuidas no lugar do 500
+      ctx.beginPath();
+      ctx.clearRect(0, 0, 500, 500);
+      quadro.dataset = {};
+      console.log(quadro.dataset);
+    };
+  }
+
+  canvas_control(signatureTechnician);
+  canvas_control(signatureClient);
+
 };
+
+
+
+function posicao(obj) {
+ // console.log(obj)
+  var coordenadas = obj.getBoundingClientRect();
+  var x = coordenadas.left;
+  var y = coordenadas.top;
+  var coo = {
+    "x": x,
+    "y": y,
+  };
+  return coo;
+}
